c16dabc06d334ca63340da40734cbb98e7eee707
romaincaraes <hello@romaincaraes.fr>
2020-08-18 20:47:41 +0200

    Update bond.py
    Define get_coupon_dates() method in Bond class

--- bond.py
+++ bond.py
@@ -25,7 +25,14 @@ class Bond() :
 		return ttm
 
 	def get_coupon_dates(self) :
-		return
+		coupon_dates = []
+		maturity = datetime.datetime.strptime(self.maturity, "%Y-%m-%d")
+		ttm = self.get_term_to_maturity("y")
+		for t in range(1, ttm * self.frequency + 1) :
+			date = (maturity - datetime.timedelta(days=((365.25 * t) / self.frequency))).strftime("%Y-%m-%d")
+			coupon_dates.append(date)
+		coupon_dates = pd.DataFrame(coupon_dates[::-1]).set_index(0)
+		return coupon_dates
 
 	def get_price(self, ytm) :
 		price = 0
