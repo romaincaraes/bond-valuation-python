7e88ffe1fada30daa1d2f121e3872a2829e2ed45
romaincaraes <hello@romaincaraes.fr>
2020-10-23 19:32:01 +0200

    Fix indentation, spaces and tabulations

--- bond-valuation/bond.py
+++ bond-valuation/bond.py
@@ -7,69 +7,69 @@ import pandas as pd
 import streamlit as st
 
 class Bond() :
-	def __init__(self, par, coupon, frequency, maturity) :
-		self.par = par
-		self.coupon = coupon
-		self.frequency = frequency
-		self.maturity = maturity
+    def __init__(self, par, coupon, frequency, maturity) :
+        self.par = par
+        self.coupon = coupon
+        self.frequency = frequency
+        self.maturity = maturity
 
-	def get_term_to_maturity(self, unit="y") :
-		today = datetime.datetime.now()
-		maturity = datetime.datetime.strptime(self.maturity, "%Y-%m-%d")
-		ttm = (maturity - today)
-		if (unit == "d") :
-			ttm = ttm.days
-		elif (unit == "m") :
-			ttm = round(ttm.days/30)
-		elif (unit == "y") :
-			ttm = round(ttm.days/360)
-		return ttm
+    def get_term_to_maturity(self, unit="y") :
+        today = datetime.datetime.now()
+        maturity = datetime.datetime.strptime(self.maturity, "%Y-%m-%d")
+        ttm = (maturity - today)
+        if (unit == "d") :
+            ttm = ttm.days
+        elif (unit == "m") :
+            ttm = round(ttm.days/30)
+        elif (unit == "y") :
+            ttm = round(ttm.days/360)
+        return ttm
 
-	def get_coupon_dates(self) :
-		coupon_dates = []
-		maturity = datetime.datetime.strptime(self.maturity, "%Y-%m-%d")
-		ttm = self.get_term_to_maturity("y")
-		for t in range(1, ttm * self.frequency + 1) :
-			date = (maturity - datetime.timedelta(days=((365.25 * t) / self.frequency))).strftime("%Y-%m-%d")
-			coupon_dates.append(date)
-		return coupon_dates
+    def get_coupon_dates(self) :
+        coupon_dates = []
+        maturity = datetime.datetime.strptime(self.maturity, "%Y-%m-%d")
+        ttm = self.get_term_to_maturity("y")
+        for t in range(1, ttm * self.frequency + 1) :
+            date = (maturity - datetime.timedelta(days=((365.25 * t) / self.frequency))).strftime("%Y-%m-%d")
+            coupon_dates.append(date)
+        return coupon_dates
 
-	def get_price(self, ytm) :
-		price = 0
-		ttm = self.get_term_to_maturity("y")
-		cashflow = (self.par * self.coupon) / self.frequency
-		ytm /= self.frequency
-		for t in range(1, ttm * self.frequency + 1) :
-			if (t == ttm * self.frequency) :
-				present_value = (self.par + cashflow) / ((1 + ytm) ** t)
-			else :
-				present_value = cashflow / ((1 + ytm) ** t)
-			price += present_value
-		return price
+    def get_price(self, ytm) :
+        price = 0
+        ttm = self.get_term_to_maturity("y")
+        cashflow = (self.par * self.coupon) / self.frequency
+        ytm /= self.frequency
+        for t in range(1, ttm * self.frequency + 1) :
+            if (t == ttm * self.frequency) :
+                present_value = (self.par + cashflow) / ((1 + ytm) ** t)
+            else :
+                present_value = cashflow / ((1 + ytm) ** t)
+            price += present_value
+        return price
 
-	def get_cashflows(self, ytm) :
-		cf = []
-		ttm = self.get_term_to_maturity("y")
-		cashflow = (self.par * self.coupon) / self.frequency
-		ytm /= self.frequency
-		for t in range(1, ttm * self.frequency + 1) :
-			if (t == ttm * self.frequency) :
-				present_value = (self.par + cashflow) / ((1 + ytm) ** t)
-				cashflow = self.par + cashflow
-			else :
-				present_value = cashflow / ((1 + ytm) ** t)
-			cf.append([t, cashflow, present_value])
-		ytm *= self.frequency
-		df = pd.DataFrame((cf), columns=["T", "CF", "PV"])
-		df['date'] = self.get_coupon_dates()[::-1]
-		df = df[["T", "date", "CF", "PV"]].set_index("T")
-		return df
+    def get_cashflows(self, ytm) :
+        cf = []
+        ttm = self.get_term_to_maturity("y")
+        cashflow = (self.par * self.coupon) / self.frequency
+        ytm /= self.frequency
+        for t in range(1, ttm * self.frequency + 1) :
+            if (t == ttm * self.frequency) :
+                present_value = (self.par + cashflow) / ((1 + ytm) ** t)
+                cashflow = self.par + cashflow
+            else :
+                present_value = cashflow / ((1 + ytm) ** t)
+            cf.append([t, cashflow, present_value])
+        ytm *= self.frequency
+        df = pd.DataFrame((cf), columns=["T", "CF", "PV"])
+        df['date'] = self.get_coupon_dates()[::-1]
+        df = df[["T", "date", "CF", "PV"]].set_index("T")
+        return df
 
-	def get_current_yield(self, ytm) :
-		cashflow = self.par * self.coupon
-		price = self.get_price(ytm)
-		current_yield = cashflow / price
-		return current_yield
+    def get_current_yield(self, ytm) :
+        cashflow = self.par * self.coupon
+        price = self.get_price(ytm)
+        current_yield = cashflow / price
+        return current_yield
 
 def user_input_features() :
     st.sidebar.header("Bond")
@@ -77,7 +77,7 @@ def user_input_features() :
     coupon = st.sidebar.number_input("Annual Coupon (%)", 0.00, 100.00, 1.50, step=0.05)
     frequency = st.sidebar.selectbox("Frequency", [1, 2, 4, 12], 3)
     maturity = st.sidebar.date_input("Maturity", datetime.date(2030, 12, 31))
-    
+
     st.sidebar.header("Market")
     rate = st.sidebar.number_input("Market Rate (%)", 0.00, 100.00, 1.00, step=0.05)
 
@@ -91,7 +91,7 @@ def user_input_features() :
     features = pd.DataFrame(data, index=[0])
     return features
 
-st.subheader("User Input Parameters")    
+st.subheader("User Input Parameters")
 features = user_input_features()
 st.write(features)
 
@@ -112,7 +112,7 @@ current_yield = round(bond.get_current_yield(rate), 6)
 st.write("Current Yield", current_yield*100, "%")
 
 def main() :
-	pass
+    pass
 
 if __name__ == "__main__" :
-	main()
+    main()
