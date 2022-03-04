45ac853f470663824a1548bc73b1a2e2fcfc36cd
romaincaraes <hello@romaincaraes.fr>
2022-03-05 00:07:11 +0100

    Update bond-valuation/tests/
    Add test_get_coupon_dates() to test_bond.py

--- bond-valuation/tests/test_bond.py
+++ bond-valuation/tests/test_bond.py
@@ -7,3 +7,6 @@ def test_bond(bond):
 
 def test_get_term_to_maturity(bond):
      assert isinstance(bond.get_term_to_maturity(), int) and bond.get_term_to_maturity() > 0
+
+def test_get_coupon_dates(bond):
+    assert isinstance(bond.get_coupon_dates(), list)
