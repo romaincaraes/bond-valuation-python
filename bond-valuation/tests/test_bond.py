c34c27d4fc97a17394aa29e66ce16d8e963acc4d
romaincaraes <hello@romaincaraes.fr>
2022-03-10 14:12:44 +0100

    Update bond-valuation/tests/
    Update test_get_term_to_maturity()

--- bond-valuation/tests/test_bond.py
+++ bond-valuation/tests/test_bond.py
@@ -7,7 +7,8 @@ def test_bond(bond):
     assert isinstance(bond, Bond)
 
 def test_get_term_to_maturity(bond):
-     assert isinstance(bond.get_term_to_maturity(), int) and bond.get_term_to_maturity() > 0
+     assert isinstance(bond.get_term_to_maturity(), int)
+     assert bond.get_term_to_maturity() > 0
 
 def test_get_coupon_dates(bond):
     assert isinstance(bond.get_coupon_dates(), list)
